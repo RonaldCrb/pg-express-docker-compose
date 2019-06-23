22febee0e215c8b6b30d24f5d84a387c7c7d8516
Ron Alonzo <alonzo.ronald@gmail.com>
2019-06-23 00:58:09 -0400

    added port

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 
 const dbConfig = {
   dialect: 'postgres',
-  host: "db"
+  host: "db",
+  port: 5432
 }
 
 const sequelize = new Sequelize('postgres', 'postgres', 'example', dbConfig)
